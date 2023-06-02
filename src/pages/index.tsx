3beb957f2f5af06d313e5e42d0c54ef2ad253cdb
Fernando Dorantes <fdorantesm@gmail.com>
2023-06-02 12:33:34 -0600

    chore: add hof to handle missing token and remove lorem from home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,19 @@
 import { Page } from "src/components/page";
 import { withAuth } from "src/hofs/with-auth";
 import { Layout } from "src/layout/default";
-import { health } from "src/services/api/health/health.service";
 
 export default function Home() {
   return (
     <>
       <Layout>
-        <Page title="Home">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia unde
-            autem vel error at molestias voluptatem neque, exercitationem
-            mollitia assumenda vitae dignissimos deleniti est optio rerum
-            laudantium deserunt adipisci quidem!
-          </p>
-        </Page>
+        <Page title="Home" children={undefined}></Page>
       </Layout>
     </>
   );
 }
+
+export const getServerSideProps = withAuth((ctx: any) => {
+  return {
+    props: {},
+  };
+});
