4d62256069553acd6622f85696a5033d86269451
Fernando Dorantes <fdorantesm@gmail.com>
2023-06-02 01:40:37 -0600

    fix: disable devtools in production mode

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,5 @@
 export const config = {
+  nodeEnv: process.env.NODE_ENV,
   primaryColor: "#555",
   baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3500",
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,25 +1,25 @@
-import thunk from 'redux-thunk';
-import { createWrapper } from 'next-redux-wrapper';
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import thunk from "redux-thunk";
+import { createWrapper } from "next-redux-wrapper";
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-import { rootReducer } from './modules/root.reducer';
+import { rootReducer } from "./modules/root.reducer";
+import { config } from "src/config/config";
 
 const persistConfig = {
-  key: 'root',
-  storage
+  key: "root",
+  storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  devTools: true,
+  devTools: config.nodeEnv !== "production",
   reducer: persistedReducer,
   preloadedState: {},
-  middleware: [thunk]
+  middleware: [thunk],
 });
 
-// assigning store to next wrapper
 export const persistor = persistStore(store);
 export const wrapper = createWrapper(() => store);
